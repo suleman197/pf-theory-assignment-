d10b320693182aa046e6dd204a72ca4ce0574758
Muhammad Suleman <sulemanmunir6752@gmail.com>
2026-04-14 17:18:36 +0500

    pf programs 1.cpp

---
+++ pf programs  1.cpp
@@ -0,0 +1,27 @@
+// 1 program
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Enter value for y = ";
+	cin>>y;
+	if (y==10)
+	x=0;
+	else
+	x=1;
+	
+	cout<<"value of x = " <<x;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
