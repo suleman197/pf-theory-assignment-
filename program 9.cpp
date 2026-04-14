a0532d7a61e9a225092f93d36e47e4d0163ee38f
Muhammad Suleman <sulemanmunir6752@gmail.com>
2026-04-14 17:08:02 +0500

    Create program 9.cpp

---
+++ program 9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    char ch;
+
+    for(ch='A'; ch<='Z'; ch++) {
+        cout << ch << " = " << (int)ch << endl;
+    }
+
+    return 0;
+}
