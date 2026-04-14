854c453d92aff4f8d899de5fdc5346b435c3565b
Muhammad Suleman <sulemanmunir6752@gmail.com>
2026-04-14 17:04:46 +0500

    Create program 4.cpp

---
+++ program 4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      double a =10;
+       double b =10;
+       
+       a = a + 0.2;
+       
+    cout<<" a = "<< a << endl;
+    cout<<" b = "<< b << endl;
+    
+    if( a>b)
+    cout<<" A is grater and b is smaller";
+    else
+     cout<<" B is grater and a is smaller";
+       
+	return 0;
+}
